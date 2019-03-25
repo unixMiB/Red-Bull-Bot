ea5f10e62ee879212ecec91bf080fa0f8bddaccd
Cristian Baldi <bld.cris.96@gmail.com>
2019-03-25 13:13:38 +0100

    Improve code, add better filename

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import os
-import datetime 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import datetime 
+from pathlib import Path
+
+# slugify
+import unicodedata
+import re
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,49 +19,77 @@ logger = logging.getLogger("RedBullBot")
 
 def start(bot, update):
     """Send a message when the command /start is issued."""  
-    logger.info(update.message.from_user.id)
-    update.message.reply_text('Ciao, mandami una foto mentre bevi una Redbull', quote=True)
+    logger.info(f"Start issued by: {get_user_name(update.message.from_user)}")
+    update.message.reply_text('Ciao, mandami una foto mentre bevi una Red Bull', quote=True)
 
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Mandami una foto mentre bevi una RedBull', quote=True)
+    logger.info(f"Help issued by: {get_user_name(update.message.from_user)}")
+    update.message.reply_text('Mandami una foto mentre bevi una Red Bull', quote=True)
 
 
-def tgphoto(bot, update):
+def slugify(value, allow_unicode=False):
+    """
+    https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces to hyphens.
+    Remove characters that aren't alphanumerics, underscores, or hyphens.
+    Convert to lowercase. Also strip leading and trailing whitespace.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value).strip().lower()
+    return re.sub(r'[-\s]+', '-', value)
+
+def get_user_name(user):
+    name = None
+    if user.username:
+        name = user.username
+    else:
+        name = user.first_name
+        if user.last_name:
+            name = f"{name} {user.last_name}"
+    return name
+
+def save_photo(bot, update):
+    logger.info(f"Photo sent by: {get_user_name(update.message.from_user)}")
     file_id = update.message.photo[-1].file_id
-    newFile = bot.get_file(file_id)
-    update.message.reply_text("Grazie per la foto, goditi la tua RedBull", quote=True)
-    print(update)
-    now = datetime.datetime.now()
-    timeStamp = now.strftime('%d_%m_%Y_%H-%M')
-    fileName = str(update.message.from_user.username)+ "_" + str(timeStamp)+ str(update.message.message_id)+'.png'
-    newFile.download("./pic/"+fileName)
+    photo = bot.get_file(file_id)
+    update.message.reply_text("Grazie per la foto, goditi la tua Red Bull!", quote=True)
+    timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    valid_user_name = slugify(get_user_name(update.message.from_user))
+    filename = f"{timestamp}_{valid_user_name}_{update.message.message_id}.png"
+    photo.download(Path(f"./pic/{filename}"))
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
+    logger.error(f'Update "{update}" caused error "{error}"')
 
 def main():
     """Start the bot."""
-    token = os.environ.get("BOT_TOKEN")
-    if token is None:
+    logger.info("The bot is starting...")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    if BOT_TOKEN is None:
         logger.critical("Please set the BOT_TOKEN env variable")
         return 
 
-    updater = Updater(token)
+    updater = Updater(BOT_TOKEN)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
 
     # photo handler
-    dp.add_handler(MessageHandler(Filters.photo, tgphoto))
+    dp.add_handler(MessageHandler(Filters.photo, save_photo))
 
     dp.add_error_handler(error)
+
+    logger.info("The bot is listening...")
     updater.start_polling()
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
